54b6efce2a77c22b71f0c4565084b6ce600c29a4
duy nguyen <nguyenmaukhanhduy07a5@gmail.com>
2021-09-15 17:35:25 +0700

    Excercise2

---
+++ Excercise2/Excercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Excercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input A = ");
+                float a = float.Parse(Console.ReadLine());
+                Console.Write("Input B = ");
+                float b = float.Parse(Console.ReadLine());
+                Console.Write("Summation = ");
+                Console.WriteLine(a + b);
+                Console.Write("Subtraction = ");
+                Console.WriteLine(a - b);
+                Console.Write("Multiplication = ");
+                Console.WriteLine(a * b);
+                Console.Write("Division = ");
+                Console.WriteLine(a / b);
+        }
+    }
+}
