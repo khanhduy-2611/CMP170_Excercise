8e6e9c38722694f9c758fac75bcef50c4c50ae1e
duy nguyen <nguyenmaukhanhduy07a5@gmail.com>
2021-09-24 23:44:01 +0700

    Topic5

---
+++ Topic5/ListAllFileInDocument/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ListAllFileInDocument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            ListAllFileInYourMyDocuments(@"C:\Users\ACER\Downloads\toppic5.2");
+            Console.WriteLine("Press Enter to continute:");
+            Console.ReadLine();
+        }
+        static void ListAllFileInYourMyDocuments(string WorkingMyDocuments)
+        {
+            string[] filePaths = Directory.GetFiles(WorkingMyDocuments);
+
+            foreach (string filePath in filePaths)
+            {
+                Console.WriteLine(filePath);
+            }
+        }
+    }
+}
---
+++ Topic5/ReadTextFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ReadTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Something to file: ");
+            string userName = Console.ReadLine();
+
+            string[] lines = { userName };
+            File.WriteAllLines(@"C:\Users\ACER\Downloads\texttoppic5\toppic5.txt", lines);
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+            Console.WriteLine("the contents of the file: ");
+            string[] readText = File.ReadAllLines(@"C:\Users\ACER\Downloads\texttoppic5\toppic5.txt");
+            foreach (string s in readText)
+            {
+                Console.WriteLine("\t" + s);
+            }
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
