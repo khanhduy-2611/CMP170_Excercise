aac28169a786ad02a4b3be7ee64f9d3ed7a0a901
duy nguyen <nguyenmaukhanhduy07a5@gmail.com>
2021-10-17 20:52:34 +0700

    Excercise1-2

--- Practice03/Practice03/SearchStudentViewModel.cs
+++ Practice03/Practice03/SearchStudentViewModel.cs
@@ -1,76 +1,79 @@
-﻿using System;
+﻿using Practice03;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Collections.ObjectModel;
-using System.Windows.Input;
 using System.Text.Json;
-
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Practice03
 {
-    class SearchStudentViewModel : BaseViewModel
+    public class SearchStudentViewModel : BaseViewModel
     {
         public class Student
         {
             public int studentId { get; set; }
-            public String firstname { get; set; }
-            public String lastname { get; set; }
-            public String email { get; set; }
-            public String gender { get; set; }
-            public String Class { get; set; }
+            public string firstname { get; set; }
+            public string lastname { get; set; }
+            public string email { get; set; }
+            public string gender { get; set; }
+            public string Class { get; set; }
             public decimal gpa { get; set; }
         }
-        private String m_Searchkeyword;
+        private string m_searchKeyword;
+        public string SearchKeyword
+        {
+            get => m_searchKeyword;
+            set
+            {
+                m_searchKeyword = value;
+                OnPropertyChanged(nameof(SearchKeyword));
+            }
+        }
+        private string m_selectedClass;
+        public string SelectedClass
+        {
+            get => m_selectedClass;
+            set
+            {
+                m_selectedClass = value;
+                OnPropertyChanged(nameof(SelectedClass));
+            }
+        }
+        private Student m_selectedStudent;
+        public Student SelectedStudent
+        {
+            get => m_selectedStudent;
+            set
+            {
+                m_selectedStudent = value;
+                OnPropertyChanged(nameof(SelectedStudent));
+            }
+        }
 
-        public String Searchkeyword
+        public void DoOpenDetail()
         {
-            get => m_Searchkeyword;
-            set
-            {
-                m_Searchkeyword = value;
-                OnPropertyChanged(nameof(Searchkeyword));
-            }
-        }
-        private String m_selectedclass;
-        public String SelectedClass
-        {
-            get => m_Searchkeyword;
-            set
-            {
-                m_selectedclass = value;
-                OnPropertyChanged(nameof(m_selectedclass));
-            }
-        }
-        private String m_selectedstudent;
-        public String SelectedStudent
-        {
-            get => m_selectedstudent;
-            set
-            {
-                m_selectedstudent = value;
-                OnPropertyChanged(nameof(m_selectedstudent));
-            }
+            var StudentDetailViewModel1 = new StudentDetailViewModel(SelectedStudent);
+            Window1 window1 = new Window1();
+            window1.DataContext = StudentDetailViewModel1;
+            window1.ShowDialog();
         }
 
         public ObservableCollection<Student> Students { get; set; }
         public ICommand SearchCommand { get; set; }
         public ICommand ResetCommand { get; set; }
         public ICommand OpenDetailCommand { get; set; }
-
-
         public SearchStudentViewModel()
         {
             var jsonString = File.ReadAllText("Student_Data.json");
             var students = JsonSerializer.Deserialize<List<Student>>(jsonString);
             Students = new ObservableCollection<Student>(students);
+            OpenDetailCommand = new ConditionalCommand(x => DoOpenDetail());
+        }
 
-        }
-        public override string ToString()
-        {
-            return base.ToString();
-        }
+
     }
 }
